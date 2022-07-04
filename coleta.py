2c162319092e441c4e4007f896f7833b0b98cd22
Lucas Miranda <77032413+LucasMirandaVS@users.noreply.github.com>
2022-07-04 19:54:27 -0300

    Update coleta.py
    atualizando o trabalho

--- coleta.py
+++ coleta.py
@@ -1,5 +1,14 @@
-# Coletando dados pela API do governo
-
-import pandas as pd
-import matplotlib.pyplot as plt
 import wbgapi as wb
+import pandas as pd
+
+df1 =  wb.data.DataFrame(['NY.GDP.DEFL.KD.ZG.AD'], 
+                    economy = wb.region.members('LCN'), # América Latina e Caribe
+                    time=range(1995, 2021),
+                    labels = True,
+                    skipAggs=True, 
+                    skipBlanks=True, 
+                    columns='series').reset_index()
+
+df1_1= pd.pivot_table(df1, values='NY.GDP.DEFL.KD.ZG.AD', index = 'economy', columns='time').dropna()
+
+df1_1.to_csv('Inflation, GDP deflator(annual %).csv')
